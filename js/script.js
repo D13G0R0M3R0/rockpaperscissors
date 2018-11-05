1ae39ac20c433db1f60da66ba75acdf05dcbdd0f
Diego Romero <poke48369@gmail.com>
2018-11-05 23:04:24 +0000

    hi

--- js/script.js
+++ js/script.js
@@ -5,7 +5,14 @@
 //GLOBAL VARIABLES
 /* global $ */
 
-
-
+$("#shoot").click(function() {
+    $("#userChoice").text($("#input").val());
+});
+$("#shoot").click(function() {
+    $("#computerChoice").text($("#").val());
+});
+var userChoice =
+var computerChoice =
+var winner =
 // DOCUMENT READY FUNCTION BELOW
 
